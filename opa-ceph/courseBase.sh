e067446c3399226d1f8c6d0f72cbe7ffad817bbb
Ashutosh Narkar <anarkar4387@gmail.com>
2019-05-17 15:31:28 -0700

    Re-tag Ceph latest-nautilus image
    Signed-off-by: Ashutosh Narkar <anarkar4387@gmail.com>

--- opa-ceph/courseBase.sh
+++ opa-ceph/courseBase.sh
@@ -1,2 +1,2 @@
 ssh root@host01 "minikube start && eval $(minikube docker-env)"
-ssh root@host01 "docker pull openpolicyagent/rook-operator:latest && ceph/daemon-base:latest-master"
+ssh root@host01 "docker pull openpolicyagent/rook-operator:latest && openpolicyagent/ceph-daemon-base:latest-nautilus"
